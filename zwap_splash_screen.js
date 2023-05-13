7d29ea16d8ed3dab1c8f9307b7287867d5b857ba
taa <luigi@zwap.in>
2023-05-13 11:56:40 +0200

    first commit

---
+++ zwap_splash_screen.js
@@ -0,0 +1,35 @@
+
+class ZwapSplashScreen{
+
+    constructor() {
+        this.basePath = "http://0.0.0.0:8000";
+    }
+
+    loadSplash() {
+        let originalHtml = document.body.innerHTML;
+        document.body.innerHTML = '';
+        fetch(`${this.basePath}/splash.html`)
+            .then(response => response.text())
+            .then(html => {
+                const splashElement = document.createElement('div');
+                splashElement.innerHTML = html;
+                document.body.appendChild(splashElement);
+                fetch(`${this.basePath}/zwap_loader.gif`)
+                    .then(response => response.blob())
+                    .then(blob => {
+                        const imageUrl = URL.createObjectURL(blob);
+                        const logoImage = splashElement.querySelector('.logo-image');
+                        logoImage.src = imageUrl;
+                        window.addEventListener('app_loaded', function () {
+                            const originalElement = document.createElement('div');
+                            originalElement.innerHTML = originalHtml;
+                            document.body.innerHTML = originalElement.innerHTML;
+                            splashElement.classList.add('hide');
+                        });
+                    });
+            });
+    }
+    hideSplash() {
+        window.dispatchEvent(new Event('app_loaded'));
+    }
+}
